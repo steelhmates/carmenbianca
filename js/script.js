33b44ebee0d3b0c77279be6b47809ffd76d18c07
Patrick Madueke <paredge@yahoo.com>
2023-11-01 07:39:16 +0100

    +U

--- js/script.js
+++ js/script.js
@@ -2,13 +2,58 @@
 function print(s) {
 	console.log(s);
 }
-
-$(document).ready(function(){
-	
-	var s = $('.roleName').eq(0).text();
-	if (s === "Narrator - ") {
-		var today = new Date().toLocaleDateString();
-		
+var colors = [
+	'#fefed8',
+	'#dbffdb',
+	'#ffd5d6'
+]
+function getColor(role) {
+	c = '#eee';
+	switch (role) {
+		case 'God': c = colors[0]; break;
+		case 'Narrator': c = colors[1]; break;
+		case '': c = colors[2]; break;
+		default: break;
 	}
-	print(s);
+	return c;
+}
+$(document).ready(function(){
+	if ($('.role')) {
+		var pageStr = $('#page').attr('name');
+		print(pageStr);
+		var page = parseInt(pageStr, 10);
+		var nUrl = '../roles/'+page+'.txt';
+		$.ajax({type: 'GET', url: nUrl, async: true,
+			success: function(result) {
+				var list = result.replace("\r", "").split('\n');
+				var lDoc = $('.role');
+				print(lDoc.length);
+				for (i=0; i<Math.min(lDoc.length, list.length); i++) {
+					var role = list[i].trim(); // Trim required.
+					var rText = lDoc.eq(i).find('.roleText').first();
+					var rName = lDoc.eq(i).find('.roleName').first();
+					if (role === 'Narrator') {
+						rName.html("Narrator<span> - Year 722<span class='verify'>3</span>, 1st November, 2023</span>");
+					} else {
+						rName.html(role);
+					}					
+					rText.css('background-color', getColor(role));
+				}
+			}
+		});
+	}
 });
+
+/*
+var today = new Date().toLocaleDateString();
+
+error: function (jqXHR, textStatus, errorThrown) {
+	console.log(jqXHR)
+},
+
+.find(...)
+
+$.each([ 52, 97 ], function( index, value ) {
+  alert( index + ": " + value );
+});
+*/
